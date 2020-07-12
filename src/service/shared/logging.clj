0f453341cc24378a790351ccee3e2f1aff345ec8
Toby Clemson <tobyclemson@gmail.com>
2020-07-12 14:34:18 +0100

    Capture System.out and System.err to logs.

--- src/service/shared/logging.clj
+++ src/service/shared/logging.clj
@@ -1,11 +1,13 @@
 (ns service.shared.logging
   (:require
     [cambium.codec :as codec]
-    [cambium.logback.json.flat-layout :as flat])
+    [cambium.logback.json.flat-layout :as flat]
+    [clojure.tools.logging :as tools-logging])
   (:import [org.slf4j.bridge SLF4JBridgeHandler]))
 
 (defn initialise []
   (do
+    (tools-logging/log-capture! *ns*)
     (flat/set-decoder! codec/destringify-val)
     (SLF4JBridgeHandler/removeHandlersForRootLogger)
     (SLF4JBridgeHandler/install)))
