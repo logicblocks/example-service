7eb575baf7f93d28cfe8c2c635d7957373eb4590
Toby Clemson <tobyclemson@gmail.com>
2020-07-12 16:06:54 +0100

    Add file logger for running server. Remove standard streams capture.

--- src/service/shared/logging.clj
+++ src/service/shared/logging.clj
@@ -1,13 +1,11 @@
 (ns service.shared.logging
   (:require
     [cambium.codec :as codec]
-    [cambium.logback.json.flat-layout :as flat]
-    [clojure.tools.logging :as tools-logging])
+    [cambium.logback.json.flat-layout :as flat])
   (:import [org.slf4j.bridge SLF4JBridgeHandler]))
 
 (defn initialise []
   (do
-    (tools-logging/log-capture! *ns*)
     (flat/set-decoder! codec/destringify-val)
     (SLF4JBridgeHandler/removeHandlersForRootLogger)
     (SLF4JBridgeHandler/install)))
